ecce2eab3d3a042992701f8058ac060c9e8b7108
Cicerofer <ciceroferreira@hotmail.com>
2023-12-17 20:46:18 -0300

    projejo deafio concluido

---
+++ consoleTeste.cs
@@ -0,0 +1,19 @@
+
+using System;
+
+namespace consoleTest
+{
+    using Terminal.Gui;
+
+    class Demo {
+        static int Main ()
+        {
+            Application.Init ();
+
+            var n = MessageBox.Query (50, 7,
+                "Question", "Do you like console apps?", "Yes", "No");
+
+            return n;
+        }
+    }
+}
